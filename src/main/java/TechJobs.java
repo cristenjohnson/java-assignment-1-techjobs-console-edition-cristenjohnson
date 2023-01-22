7785768f696c4a5b5f2ffa88d42554c867ee348a
Brownie <cristenjohnson@rocketmail.com>
2023-01-22 04:36:04 -0600

    added loop

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -120,6 +117,24 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+//
+            for(HashMap<String,String> jobs : someJobs){
+                System.out.println();
+                System.out.println("*****");
+                for (String key : jobs.keySet()) {
+                    System.out.println(key + ": "+ jobs.get(key));
+                }
+                System.out.println("*****");
+                System.out.println();
+
+            }
+        }
+
+
+
+
     }
-}
+
+//    private static int keySet() {
+//    }
+//}
